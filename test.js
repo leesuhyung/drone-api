81398d332b9c842d0d3c6f364c240324da9da234
shlee <realnavystyle@gmail.com>
2019-08-02 13:07:36 +0900

    .

--- test.js
+++
@@ -1,9 +0,0 @@
-const winner = 1019.4;
-const loser = 995.2;
-const K = 10;
-const elo = K * (1 - (1 / (1 + (Math.pow(10, (loser - winner) / 400)))).toFixed(2));
-console.log(elo);
-console.log('1', 1 / (1 + (Math.pow(10, (loser - winner) / 400))));
-console.log('2', 1 / (1 + (Math.pow(10, (loser - winner) / 400))));
-console.log('3', 1 - (1 / (1 + (Math.pow(10, (loser - winner) / 400)))));
-console.log('4', (K * (1 - (1 / (1 + (Math.pow(10, (loser - winner) / 400)))))).toFixed(1));
